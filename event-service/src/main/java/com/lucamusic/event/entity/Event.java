dd14c3f6cda74bbcf57fe2ca7816b0c6ab1ec7f1
Julio <julio@ruedaferrero.com>
2021-09-14 10:46:15 +0200

    Updated event entity v3

--- event-service/src/main/java/com/lucamusic/event/entity/Event.java
+++ event-service/src/main/java/com/lucamusic/event/entity/Event.java
@@ -1,7 +1,10 @@
 package com.lucamusic.event.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
@@ -9,9 +12,10 @@ import java.util.Date;
 import java.util.Map;
 
 @Data
-@Document(value = "events") @Builder
+@Document(value = "events") @Builder @AllArgsConstructor @NoArgsConstructor
 public class Event {
-	private String id;
+	@MongoId
+	private ObjectId id;
 	private String name;
 	private String shortDescription;
 	private String longDescription;
