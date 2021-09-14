299d4cab4a89da182df48f73ea97caa610663411
Julio <julio@ruedaferrero.com>
2021-09-14 08:48:33 +0200

    Redo user-service module

---
+++ user-service/src/main/java/com/lucamusic/user/entity/User.java
@@ -0,0 +1,52 @@
+package com.lucamusic.user.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	Long id;
+	String fullName;
+	String email;
+	String password;
+	Date registerDate;
+
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public User(Long id, String fullName, String email, String password, Date registerDate) {
+		super();
+		this.id = id;
+		this.fullName = fullName;
+		this.email = email;
+		this.password = password;
+		this.registerDate = registerDate;
+	}
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ user-service/src/test/java/com/lucamusic/user/UserTest01_Autowired.java
@@ -0,0 +1,5 @@
+package com.lucamusic.user;
+
+public class UserTest01_Autowired {
+
+}
